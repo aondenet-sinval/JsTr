a2af1e2c4b45f8d266ea0a97aee11bbac6bcfe3b
aondenet-sinval <sinvalsatv@gmail.com>
2022-07-25 16:13:52 -0300

    controle de bubling

--- 4Js-web/eventos/ev_5.js
+++ 4Js-web/eventos/ev_5.js
@@ -3,11 +3,20 @@
 let btn = document.querySelector('button');
 let video = document.querySelector('video');
 let videoBox = document.querySelector('div');
+let header = document.querySelector('header');
+
+//Função para teste de bubling
+header.onclick = function(){
+  videoBox.setAttribute('class', 'hidden');
+}
 
 btn.onclick = function(){
   videoBox.setAttribute('class', 'showing');
 };
 
-video.onclick = function(){
+video.onclick = function(e){
   video.play();
+  e.stopPropagation();
+  //bloqueando a propagação do clique
+  //para o elemento html superior header
 };
