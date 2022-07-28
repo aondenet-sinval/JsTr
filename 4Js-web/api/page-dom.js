847c892cdc00ff3ec67b2069c6c07700329725aa
aondenet-sinval <sinvalsatv@gmail.com>
2022-07-27 21:36:29 -0300

    manipulando dom...

--- 4Js-web/api/page-dom.js
+++ 4Js-web/api/page-dom.js
@@ -6,6 +6,9 @@ const secB = document.querySelector('section b');
 const btnPar = document.querySelector('.par-button button');
 const bPar = document.querySelector('b');
 const btnTexto = document.querySelector('.par-texto button');
+const btnMover = document.querySelector('.coluna-direita button');
+const textoMv = document.querySelector('.texto-movido');
+const btnRemover = document.querySelector('.remover');
 
 
 btn.onclick = function(){
@@ -17,12 +20,23 @@ btn.onclick = function(){
 
 btnPar.onclick = function(){
   const par = document.createElement('p');
-  par.textContent = 'Sem preguiça da mexer no DOM';
+  par.textContent = 'Sem preguiça da pra mexer no DOM';
   bPar.appendChild(par);
 }
-
+//Texto a ser movido definido globalmente:
+const texto = document.createTextNode('A fonte do conhecimento para essas linhas é o MDN');
 btnTexto.onclick = function(){
-  const texto = document.createTextNode('A fonte do conhecimento para essas linhas é o MDN')
   const meuTexto = document.querySelector('.bolds');
   meuTexto.appendChild(texto);
+}
+//Movendo o parágrafo criado:
+btnMover.onclick =  function(){
+  textoMv.appendChild(texto);
+}
+//Removendo o Paragrafo
+btnRemover.onclick = function(){
+  textoMv.removeChild(texto);
+  texto.remove();
+  //Implementação para navegadores mais antigos
+  //texto.parentNode.removeChild(texto);
 }
